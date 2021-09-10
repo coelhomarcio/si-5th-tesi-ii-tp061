fab86a75df4ddae4653eff80d1360375145eadb7
Marcio Coelho <marciocoelho@gmail.com>
2021-09-10 18:39:42 -0300

    Updated material.module.ts

--- src/app/module/material/material.module.ts
+++ src/app/module/material/material.module.ts
@@ -1,8 +1,20 @@
-import { NgModule }         from "@angular/core";
-import { MatToolbarModule } from "@angular/material/toolbar";
+import { NgModule }           from "@angular/core";
+import { MatButtonModule }    from "@angular/material/button";
+import { MatIconModule }      from "@angular/material/icon";
+import { MatDividerModule }   from "@angular/material/divider";
+import { MatListModule }      from "@angular/material/list";
+import { MatSidenavModule }   from "@angular/material/sidenav";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule }     from "@angular/material/input";
 
 const materials = [
-  MatToolbarModule
+  MatButtonModule,
+  MatIconModule,
+  MatDividerModule,
+  MatListModule,
+  MatSidenavModule,
+  MatFormFieldModule,
+  MatInputModule
 ];
 
 @NgModule({
