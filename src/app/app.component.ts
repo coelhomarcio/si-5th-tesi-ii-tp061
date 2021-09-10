8066576c5629a52bfca54b50ce28f69f8d5faaae
Marcio Coelho <marciocoelho@gmail.com>
2021-09-10 18:39:17 -0300

    Updated app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from "@angular/core";
 
+import { TimerService } from "./service/timer.service";
+
 @Component({
-  selector:    "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls:   ["./app.component.scss"]
+  selector: "app-root",
+  template: "<app-header></app-header>"
 })
 export class AppComponent {
-  title = "timer-disciplines";
+  title = "Temporizador com Disciplinas";
+
+  constructor(public systemTimer: TimerService) {
+    this.systemTimer.systemTimerStart(1000);
+  }
 }
